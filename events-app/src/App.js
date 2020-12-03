6afc2605286be967aaa355405ce0f6a022f972f1
cocacolaboxers <jtomasdiaz.98@icloud.com>
2020-12-03 18:29:07 -0400

    Broadened search options

--- events-app/src/App.js
+++ events-app/src/App.js
@@ -2,41 +2,39 @@ import {useState} from 'react'
 
 function App() {
   const [userInput,setUserInput] = useState(null)
-  const [eventInfo,setEventInfo] = useState([])
+  const [eventsInfo,setEventsInfo] = useState([])
 
   async function getData(){
     let message = await fetch('https://jsonplaceholder.typicode.com/photos')
     let jsonMessage = await message.json()
 
-    let eventInfo = []
+    let eventsInfo = jsonMessage.filter(event => event.title.includes(userInput));
 
-    for(let i = 0; i < 5000; i++){
-      if(jsonMessage[i].title === userInput){
-        eventInfo.push(jsonMessage[i].id)
-        eventInfo.push(jsonMessage[i].title)
-        eventInfo.push(jsonMessage[i].url)
-        eventInfo.push(jsonMessage[i].thumbnailUrl)
-        break
-      }
-    }
+    setEventsInfo(eventsInfo)
+    
+    console.log(eventsInfo)
+  }
 
-    setEventInfo(eventInfo)
-  
-    console.log(eventInfo)
+  function showEvents(){
+    return eventsInfo.map((item) => {
+      return <div>
+        <img src={item.thumbnailUrl} alt="Event cover"/>
+        <h3>Id: {item.id}</h3>
+        <h3>Title: {item.title}</h3>
+        <a href={item.url}>Click here to visit the event's URL</a>
+        <hr/>
+      </div>
+    })
   }
 
   return (
     <div className="App">
       <h1>Welcome!</h1>
-      <h2>Type the name of the event:</h2>
+      <h2>Type the name, city or category of the event:</h2>
       <input type="text" onChange={(e) => {setUserInput(e.target.value)}}/>
       <button onClick={getData}>Find</button>
       <h2>Here's what we found for: {userInput}</h2>
-      <img src={eventInfo[3]} alt="Event cover"/>
-      <h3>Id: {eventInfo[0]}</h3>
-      <h3>Title: {eventInfo[1]}</h3>
-      <h3>Url: {eventInfo[2]}</h3>
-      <a href={eventInfo[2]}>Click here to visit the event's URL</a>
+      {showEvents()}
     </div>
   );
 }
